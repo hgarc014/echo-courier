2a8bed986ff1610449c29ab757745f503bbb9cc7
Henry Garcia <01henrygarcia10@gmail.com>
2026-09-09 15:57:49 -0700

    Wire env/props sheets: restore src/entities/zones.js

--- src/entities/zones.js
+++ src/entities/zones.js
@@ -1,1 +1,15 @@
-PLACEHOLDER_ZONES
+import { state } from '../core/state.js';
+import { Entity } from './base.js';
+import { drawTiled } from '../core/sprites.js';
+import { resolveSprite } from '../core/atlas.js';
+
+export class DeliveryZone extends Entity {
+    constructor(x, y, w, h) { super(x, y, w, h, 'zone'); }
+    render(ctx) {
+        const pulse = 0.88 + 0.12 * Math.sin(state.currentTick * 0.08);
+        const img = resolveSprite(state, 'zone');
+        if (img) drawTiled(ctx, img, this.x, this.y, this.w, this.h, 40, pulse);
+        else super.render(ctx);
+        ctx.strokeStyle='#39ff14'; ctx.lineWidth=2; ctx.strokeRect(this.x, this.y, this.w, this.h);
+    }
+}
