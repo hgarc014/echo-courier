bbcae7cab7955cf84df52b14c5ce027bddcfbbde
Henry Garcia <01henrygarcia10@gmail.com>
2026-09-09 15:56:50 -0700

    Wire env/props sprite sheets into the world atlas

--- src/entities/base.js
+++ src/entities/base.js
@@ -1,11 +1,1 @@
-import { state } from '../core/state.js';
-import { drawSprite } from '../core/sprites.js';
-
-export class Entity {
-    constructor(x, y, w, h, assetName) { this.x=x; this.y=y; this.w=w; this.h=h; this.assetName=assetName; }
-    render(ctx) {
-        const img = state.assets[this.assetName];
-        if (img && drawSprite(ctx, img, this.x, this.y, this.w, this.h, { valign: 'bottom' })) return;
-        ctx.fillStyle = '#ff00ea'; ctx.fillRect(this.x, this.y, this.w, this.h);
-    }
-}
+PLACEHOLDER_BASE
