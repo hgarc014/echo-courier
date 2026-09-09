99c211eaafb5b6ce10afa5365eda1d87ef4c4ed1
Henry Garcia <01henrygarcia10@gmail.com>
2026-09-09 15:57:37 -0700

    Wire env/props sheets: restore src/entities/base.js

--- src/entities/base.js
+++ src/entities/base.js
@@ -1,1 +1,12 @@
-PLACEHOLDER_BASE
+import { state } from '../core/state.js';
+import { drawSprite } from '../core/sprites.js';
+import { resolveSprite } from '../core/atlas.js';
+
+export class Entity {
+    constructor(x, y, w, h, assetName) { this.x=x; this.y=y; this.w=w; this.h=h; this.assetName=assetName; }
+    render(ctx) {
+        const img = resolveSprite(state, this.assetName);
+        if (img && drawSprite(ctx, img, this.x, this.y, this.w, this.h, { valign: 'bottom' })) return;
+        ctx.fillStyle = '#ff00ea'; ctx.fillRect(this.x, this.y, this.w, this.h);
+    }
+}
